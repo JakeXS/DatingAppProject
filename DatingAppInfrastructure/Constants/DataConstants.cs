300a45e16788758ad690424282239d8fa031ee9a
JakeXS <deamon1888@abv.bg>
2024-03-03 21:00:05 +0200

    Constants Added

---
+++ DatingAppInfrastructure/Constants/DataConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingAppProject.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int MaxNameLength = 100;
+        public const int MaxBioLength = 500;
+    }
+}
